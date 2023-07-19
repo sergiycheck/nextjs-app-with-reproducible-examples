30c0549416128edce9ae31f39efcf369e4e8a5e7
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-07-19 14:02:19 +0300

    layout and pages

---
+++ src/app/page1/page.tsx
@@ -0,0 +1,7 @@
+export default function Page1() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold">page 1</h1>
+    </>
+  );
+}
---
+++ src/app/page2/page.tsx
@@ -0,0 +1,7 @@
+export default function Page2() {
+  return (
+    <>
+      <h1 className="text-6xl font-bold">page 2</h1>
+    </>
+  );
+}
