36e0e4489b0e07ef77a37af58069e6e0aa4b9477
Serhii Kuzmuch <sergiycheck@gmail.com>
2023-07-23 12:19:38 +0300

    fix: layer examples

--- src/app/page2/page.tsx
+++ src/app/page2/page.tsx
@@ -5,7 +5,7 @@ export default function Page2() {
 
       <div className="item">
         I am displayed in <code>color: rebeccapurple</code> because the
-        <code>special</code> layer comes after the <code>base</code> layer. My green border,
+        <code>utilities</code> layer comes after the <code>base</code> layer. My green border,
         font-size, and padding come from the <code>base</code> layer.
       </div>
     </>
